13388e8dfab484f2e821c946e4c3f572aec69d5c
Shahroz78 <shahrozkhan280@gmail.com>
2025-01-28 08:51:59 +0000

    container config done

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -8,6 +8,9 @@ let accountState;
 accountEmail = "hchc@.gmail.com"
 accountPassword = "99090"
 accountCity = "lahore"
-
+/*
+perefer not to use var
+because in issue in blockscope and functionalscope
+*/
 console.log(accountId)
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
