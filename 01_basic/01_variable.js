9a2dcc60a0e46edec66cbaa35366d30d839a90af
Shahroz78 <shahrozkhan280@gmail.com>
2025-01-28 13:20:54 +0500

    container config done

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,2 +1,5 @@
-const accountId = "12345"
-console.log("accountId");
+const accountId = 12345
+let accountEmail = "shahrozkhan280@gmail.com"
+var accountPassword = "8811"
+accountCity = "okara" 
+console.log(accountId)
